aefaa46213d56e3d2b00069c7aab0f0a383c2733
Stormsidee <delay1337pro1@mail.ru>
2025-11-10 17:24:11 +0300

    fix bugs in API start

--- start.py
+++ start.py
@@ -10,7 +10,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiki.settings')
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=7000,
         reload=True
     )
